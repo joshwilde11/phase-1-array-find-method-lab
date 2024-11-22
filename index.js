6f07abd17ea9989bc7398edfc771f350ac7e4f73
Josh Wilde <jowilde11@gmail.com>
2024-11-22 11:48:30 -0500

    done lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(records) {
+    for (const record of records) {
+        if (record.result === "W") {
+            return record.year;
+        }
+    }
+}
